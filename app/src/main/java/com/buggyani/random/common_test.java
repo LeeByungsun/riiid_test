809a142a5fd0c7d809f62a80ffa99bdf10a0a8ae
bslee <bslee@brilliantts@com>
2019-03-11 16:30:37 +0900

    random 시간 측정 테스트

--- app/src/main/java/com/buggyani/random/common_test.java
+++ app/src/main/java/com/buggyani/random/common_test.java
@@ -3,16 +3,12 @@ package com.buggyani.random;
 import java.util.Random;
 
 class common_test {
-    public static void main(String[] args) {
 
-        long start = System.currentTimeMillis();
-        // System.out.println("time = " + start);
-        int random = get_random(100000000);
-        long end = System.currentTimeMillis();
-        // System.out.println("time = " + end);
-        System.out.println("time = " + (end - start) / 1000.0);
-        System.out.println("random = " + random);
+    public static void main(String[] args) {
+        get_random_for(100000000);
+//        get_random_while(100000000);
     }
+
 
     public static int get_zero_or_one() {
         Random randomGenerator = new Random();
@@ -20,12 +16,35 @@ class common_test {
         return randomInteger;
     }
 
-    public static int get_random(int max) {
-        int num = 0;
-        for (int i = 0; i < max; i++) {
-            num += get_zero_or_one();
+    public static int get_random_for(int max) {
+        long start = System.currentTimeMillis();
+        int random = 0;
+
+//        for (int i = 0; i < max; i++) {
+//        for (int i = max; --i >= 0; ) {
+//        for (int i = max; i > 1; i--) {
+        for (int i = max - 1; i >= 0; i--) {
+            random += get_zero_or_one();
         }
-        return num;
+        long end = System.currentTimeMillis();
+        System.out.println("for time = " + (end - start) / 1000.0);
+        System.out.println("random = " + random);
+        return random;
     }
+
+    public static int get_random_while(int max) {
+        long start = System.currentTimeMillis();
+        int random = 0;
+        int i = 0;
+        while (i < max) {
+            random += get_zero_or_one();
+            i++;
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("while time = " + (end - start) / 1000.0);
+        System.out.println("random = " + random);
+        return random;
+    }
+
 
 }
